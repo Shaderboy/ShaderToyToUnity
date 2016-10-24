7f110208ae97d079859744556916edd41fdb5770
Phil Manzano <jeanllegros@yahoo.fr>
2016-10-24 13:14:39 +0200

    oops, removing duplicate
    here's a addition

--- Editor/ShadertoyToUnity.cs
+++ Editor/ShadertoyToUnity.cs
@@ -30,13 +30,11 @@ internal static class ShadertoyToUnity{
 		code = code.Replace("mod", "fmod");
 		code = code.Replace("iGlobalTime", "_Time.y");
 		code = code.Replace("fragColor =", "return");
-		code = code.Replace("iGlobalTime", "_Time.y");
 		code = code.Replace("mat2", "float2x2");
 		code = code.Replace("Texture2D", "Tex2D");
 		code = code.Replace("atan(x,y)", "atan2(y,x)");
 		code = code.Replace("*=", "mul");
 		code = code.Replace("uv.y =", "1 – uv.y");
-		code = code.Replace("fragColor =", "return");
 
 
 		Regex open = new Regex(@"mat(.)\s*(\()");
