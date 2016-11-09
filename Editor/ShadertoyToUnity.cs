0a9a141b080cdd56c74c86885e9db738f43effc5
Phil Manzano <jeanllegros@yahoo.fr>
2016-11-09 10:14:52 +0100

    sync with patch

--- Editor/ShadertoyToUnity.cs
+++ Editor/ShadertoyToUnity.cs
@@ -30,11 +30,8 @@ internal static class ShadertoyToUnity{
 		code = code.Replace("mod", "fmod");
 		code = code.Replace("iGlobalTime", "_Time.y");
 		code = code.Replace("fragColor =", "return");
-		code = code.Replace("mat2", "float2x2");
 		code = code.Replace("Texture2D", "Tex2D");
 		code = code.Replace("atan(x,y)", "atan2(y,x)");
-		code = code.Replace("*=", "mul");
-		code = code.Replace("uv.y =", "1 – uv.y");
 
 
 		Regex open = new Regex(@"mat(.)\s*(\()");
